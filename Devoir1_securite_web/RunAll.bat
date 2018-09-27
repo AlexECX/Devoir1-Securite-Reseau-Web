a69010b55f83e2a426ddd50b12a31e9767678f23
AlexECX <33321263+AlexECX@users.noreply.github.com>
2018-09-27 13:17:24 -0400

    Correction du script de lancement

--- Devoir1_securite_web/RunAll.bat
+++ Devoir1_securite_web/RunAll.bat
@@ -1,5 +1,5 @@
-start cmd /k "%~dp0Debug/Clement.exe 8812"
+start cmd /k "%~dp0x64/Debug/Clement.exe 8812"
 sleep 10
-start cmd /k "%~dp0Debug/Agnesse.exe 127.0.0.1 8812"
+start cmd /k "%~dp0x64/Debug/Agnesse.exe 127.0.0.1 8812"
 sleep 500
-start cmd /k "%~dp0Debug/Bob.exe 127.0.0.1 8812"
+start cmd /k "%~dp0x64/Debug/Bob.exe 127.0.0.1 8812"
